db6d338e4b6ca8539f0d8624d5f802a49f8f8527
Mickaël Bergem <mickael@securem.eu>
2013-06-03 21:30:56 +0200

    Affichage de la version sur A Propos...

--- apropos.php
+++ apropos.php
@@ -4,14 +4,17 @@ define('page_titre', "À propos de ce logiciel... &bull; Caisse Foyer");
 
 include_once 'inclus/tete.html.php';
 ?>
-  <div class="central bloc stocks">
+  <div class="central bloc stocks apropos">
    <a href="./index.php" class="maison">Retour</a>
     <h1>À propos de ce logiciel...</h1>
     <div class="bloc">
     Logiciel réalisé pour le foyer de l'École des Ponts, par <strong>Mickaël Bergem</strong> et <strong>Charles Bochet</strong> (promo 015) pendant leur stage chiant.
     </div>
     <div class="bloc">
-    <img src="images/logo_foyer.png" alt="" class="logo-foyer">
+    <div class="logo-foyer">
+    <img src="images/logo_foyer.png" alt="">
+    <strong class="version">Version <?php echo _VERSION; ?></strong>
+    </div>
     </div>
   </div>
 <?php
