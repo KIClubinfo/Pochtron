ad108e07cb0969b993edbcb40bd448a5d1949d9e
Charles BOCHET <pichnett.net@gmail.com>
2013-06-06 15:26:58 +0200

    Creation d'un module ajax & Commentationnage de synchro.php

--- synchro.php
+++ synchro.php
@@ -1,6 +1,6 @@
 <?php
 //Inclusions nécéssaires
-$head_HTML = '<link rel="stylesheet" href="style/ui.css"><script type="text/javascript" src="scripts/progress.js"></script>';
+$head_HTML = '<link rel="stylesheet" href="style/ui.css"><script type="text/javascript" src="scripts/progress.js"></script><script type="text/javascript" src="scripts/ajax.js"></script>';
 include_once 'inclus/tete.html.php';
 ?>
 
@@ -47,7 +47,6 @@ include_once 'inclus/tete.html.php';
 							<td><?php echo $a['prenom']; ?></td><td><?php echo $a['nom']; ?></td>
 							<td class="new_tof" data-id="<?php echo $a['id']; ?>"></td><td class="checkbox"></td>
 						</tr>
-						
 					<?php	
 					} ?>
 
@@ -70,48 +69,25 @@ var nb_done = 0;
 var nb_tot = <?php echo $sql->nbrlignes(); ?>;
 var pas = 100/nb_tot;
 
-//Parcours des élèves et recherche de photo
+//Fonction principale  Synchronisation des photos
 $("#clients tr .new_tof" ).each(function (i) 
 {
-	search_pict($(this).attr("data-id"));	
+	search_pict($(this).attr("data-id"));	//Pour chaque élève, recherche d'une photo de profil FB.
 }); 
 
-
-function ajax_url(my_url, GET_arg, callback_function, error_function)
-{
-	$.ajax({
-		url: my_url,
-        data   : {id:GET_arg['id'],fb_id:GET_arg['fb_id']},
-          cache: false,
-          dataType: "json",
-        error : function(request, error) { error_function(GET_arg['id']);   },
-        success: function(data) { callback_function(GET_arg['id'], data.code_erreur, data.reponse); }
-		   });
-	  
-}
-
-function upload_error(id)
-{
-	search_error(id);
-}
-
-function search_error(id)
-{
-	$("#clients tr .new_tof[data-id=\""+id+"\"]").html('error');
-	refresh_prog_bar();
-}
-
+//Progression & Rafrachissement de la barre de progression
 function refresh_prog_bar()
 {
 	avancement+=pas;
 	 $('#progress_bar .ui-progress').animateProgress(avancement, function() {});
 }
 
-function search_callback(id, code_erreur, reponse)
+//Fonction de callback après recherche (après réponse d'image_search.php)
+function search_callback(code_erreur, reponse, GET_args)
 {
 	if (code_erreur == "0")
 	{
-		 $("#clients tr .new_tof[data-id=\""+id+"\"]").html('<img src="https://graph.facebook.com/'+reponse+'/picture" title="Nouvelle photo" />').siblings(".checkbox").html('<input type="checkbox" checked="checked" />');     
+		 $("#clients tr .new_tof[data-id=\""+GET_args['id']+"\"]").html('<img src="https://graph.facebook.com/'+reponse+'/picture" title="Nouvelle photo" />').siblings(".checkbox").html('<input type="checkbox" checked="checked" />');     
 		 
 		 nb_done++;
 	}	 
@@ -130,12 +106,20 @@ function search_callback(id, code_erreur, reponse)
 	
 }
 
-function upload_callback(id, code_erreur, reponse)
+//Fonction de traitement d'erreur après recherche (après réponse d'image_search.php)
+function search_error(code_erreur, reponse, GET_args)
+{
+	$("#clients tr .new_tof[data-id=\""+GET_args['id']+"\"]").html(reponse);
+	refresh_prog_bar();
+}
+
+//Fonction de callback après upload (après réponse d'image_upload.php)
+function upload_callback(code_erreur, reponse, GET_args)
 {
 	if (code_erreur == "0")
 	{
-		$("#clients tr .new_tof[data-id=\""+id+"\"]").siblings(".checkbox").html('<img src="images/icones/icons/accept.png" />');
-		$("#clients tr .new_tof[data-id=\""+id+"\"]").siblings(".prec_tof").html('<img class="ico-client" src="images/photos/'+id+'.jpg" />');
+		$("#clients tr .new_tof[data-id=\""+GET_args['id']+"\"]").siblings(".checkbox").html('<img src="images/icones/icons/accept.png" />');
+		$("#clients tr .new_tof[data-id=\""+GET_args['id']+"\"]").siblings(".prec_tof").html('<img class="ico-client" src="images/photos/'+GET_args['id']+'.jpg" />');
 								 
 		nb_done++;
 	}	 
@@ -146,40 +130,45 @@ function upload_callback(id, code_erreur, reponse)
 	$("#results").text(nb_done+'/'+nb_tot+' éléments mis à jours');
 }
 
+//Fonction de traitement d'erreur après upload (après réponse d'image_upload.php)
+function upload_error(code_erreur, reponse, GET_args)
+{
+	search_error(code_erreur, reponse, GET_args);
+}
+
+//Fonction de recherche d'image
+function search_pict(my_id)
+{
+	var GET_args =  {'id' : my_id};//Arguments de la requète GET
+	ajax_url("image_search.php", GET_args, search_callback, search_error);//Appel AJAX
+}
+
+//Fonction d'upload après recherche
 function upload_pict()
 {
-	
+	//Réinitialisation de la barre de progression
 	avancement = 0;
 	nb_tot = nb_done;
 	nb_done = 0;
 	pas=100/nb_tot;
-
+	
 	$('#progress_bar .ui-progress').css('width','0');
 	$('.ui-label').css({'display':'block','cursor':'normal'}).unbind("click");
 
-	$("#clients tr .checkbox").each(function (i) 
+	
+	$("#clients tr .checkbox").each(function (i) //Pour chaque élève
 	{
-		var my_user_id=$(this).siblings(".new_tof").attr("data-id");
+		var my_user_id=$(this).siblings(".new_tof").attr("data-id");//On récupère l'id
 		var check_box = this;
 				
-		if (($(this).html() != '') && ($("input",this).prop("checked") == true))
+		if (($(this).html() != '') && ($("input",this).prop("checked") == true))//On vérifie que la check-box n'est pas cochée
 		{
-			var my_fb_id = $(this).siblings(".new_tof").children("img").attr("src").split('/')[3];
+			var my_fb_id = $(this).siblings(".new_tof").children("img").attr("src").split('/')[3];//On récupère l'id FB
 			
-			var GET_arg = new Array();
-			GET_arg['id']=my_user_id;
-			GET_arg['fb_id']=my_fb_id;
-			ajax_url("image_upload.php", GET_arg, upload_callback, upload_error);
+			var GET_args =  {'id' : my_user_id,  'fb_id':my_fb_id}; //Arguments de la requète GET
+			ajax_url("image_upload.php", GET_args, upload_callback, upload_error);//Appel AJAX
 		}
 	});
 }
-
-function search_pict(my_id)
-{
-	var GET_arg = new Array();
-	GET_arg['id']=my_id;
-	ajax_url("image_search.php", GET_arg, search_callback, search_error);
-}
-
 
 </script>
