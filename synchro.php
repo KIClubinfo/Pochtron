acdfadc255cae847225dedda0656259b9e7f6d4f
Charles BOCHET <pichnett.net@gmail.com>
2013-06-06 13:51:41 +0200

    Nettoyage page synchro.php

--- synchro.php
+++ synchro.php
@@ -1,10 +1,8 @@
 <?php
+//Inclusions nécéssaires
 $head_HTML = '<link rel="stylesheet" href="style/ui.css"><script type="text/javascript" src="scripts/progress.js"></script>';
-
 include_once 'inclus/tete.html.php';
-
-
-	?>
+?>
 
 	<div class="central bloc clients">
 		<a href="./administration.php" class="maison">Retour</a>
@@ -19,7 +17,7 @@ include_once 'inclus/tete.html.php';
 				<tr>
 					<td colspan="6" id="results">
 						<?php
-
+						//Nombre d'élèves
 						$sql->rek( 'SELECT `id` ,prenom,nom,solde,active,nb_consos FROM clients ORDER BY (solde<0) DESC, nom ASC' );
 						echo '0/'.$sql->nbrlignes().'éléments trouvés'; ?>
 					</td>
@@ -28,55 +26,134 @@ include_once 'inclus/tete.html.php';
 					<td colspan="6">
 						<div id="progress_bar" class="ui-progress-bar ui-container">
 							<div class="ui-progress" style="width: 79%;">
-								<span class="ui-label" style="display:none;">Processing <b class="value">79%</b></span>
-							</div><!-- .ui-progress -->
+								<span class="ui-label" style="display:none;">Progression <b class="value">79%</b></span>
+							</div>
 						</div>
 					</td>
 				</tr>
 			</tfoot>
 			<tbody>
 				<?php
-				
-					while($a = $sql->fetch())
+					while($a = $sql->fetch())//Boucle d'affichage des lignes
 					{
 						if(file_exists("images/photos/".$a['id'].".jpg")) 
 							$photo = $a['id'];
 						else
 							$photo = "sans_photo";
 						?>
-						<tr><td></td><td class="prec_tof"><img class='ico-client' src="images/photos/<?php echo $photo.'.jpg'; ?>" alt=''></td><td><?php echo $a['prenom']; ?></td><td><?php echo $a['nom']; ?></td>
-							
-						<?php
+						<tr>
+							<td></td>
+							<td class="prec_tof"><img class='ico-client' src="images/photos/<?php echo $photo.'.jpg'; ?>" alt=''></td>
+							<td><?php echo $a['prenom']; ?></td><td><?php echo $a['nom']; ?></td>
+							<td class="new_tof" data-id="<?php echo $a['id']; ?>"></td><td class="checkbox"></td>
+						</tr>
 						
+					<?php	
+					} ?>
 
-						echo '<td class="new_tof" data-id="'.$a['id'].'"></td><td class="checkbox"></td></tr>';
-						
-					}
-				
-				 ?>
 			</tbody>
 		</table>
 	</div>	
 
  <script type="text/javascript">
 
-var avancement = 0;
-var nb_found = 0;
-var nb_tof = <?php echo $sql->nbrlignes(); ?>;
-var pas = 100/nb_tof;
+$(function() //Chargée au chargement
+{ 
+  $('#progress_bar .ui-progress .ui-label').hide();//On masque le label de la barre de progression
+  $('#progress_bar .ui-progress').css('width', '0%');//On initialise la barre de progression à 0
 
+}); 
+
+//Intialisation de la barre de progression
+var avancement = 0;
+var nb_done = 0;
+var nb_tot = <?php echo $sql->nbrlignes(); ?>;
+var pas = 100/nb_tot;
+
+//Parcours des élèves et recherche de photo
 $("#clients tr .new_tof" ).each(function (i) 
 {
 	search_pict($(this).attr("data-id"));	
 }); 
 
+
+function ajax_url(my_url, GET_arg, callback_function, error_function)
+{
+	$.ajax({
+		url: my_url,
+        data   : {id:GET_arg['id'],fb_id:GET_arg['fb_id']},
+          cache: false,
+          dataType: "json",
+        error : function(request, error) { error_function(GET_arg['id']);   },
+        success: function(data) { callback_function(GET_arg['id'], data.code_erreur, data.reponse); }
+		   });
+	  
+}
+
+function upload_error(id)
+{
+	search_error(id);
+}
+
+function search_error(id)
+{
+	$("#clients tr .new_tof[data-id=\""+id+"\"]").html('error');
+	refresh_prog_bar();
+}
+
+function refresh_prog_bar()
+{
+	avancement+=pas;
+	 $('#progress_bar .ui-progress').animateProgress(avancement, function() {});
+}
+
+function search_callback(id, code_erreur, reponse)
+{
+	if (code_erreur == "0")
+	{
+		 $("#clients tr .new_tof[data-id=\""+id+"\"]").html('<img src="https://graph.facebook.com/'+reponse+'/picture" title="Nouvelle photo" />').siblings(".checkbox").html('<input type="checkbox" checked="checked" />');     
+		 
+		 nb_done++;
+	}	 
+	 
+	 refresh_prog_bar();
+	 
+	 $("#results").text(nb_done+'/'+nb_tot+' éléments trouvés');
+	 
+	 if (avancement >= 100)
+	 $('#progress_bar .ui-progress').animateProgress(avancement, function() 
+	 {
+		$('.ui-label').delay(1500).text('Clickez-ici pour télécharger les photos').css({'display':'block','cursor':'pointer'}).click(function(){
+			upload_pict();
+		});
+	});
+	
+}
+
+function upload_callback(id, code_erreur, reponse)
+{
+	if (code_erreur == "0")
+	{
+		$("#clients tr .new_tof[data-id=\""+id+"\"]").siblings(".checkbox").html('<img src="images/icones/icons/accept.png" />');
+		$("#clients tr .new_tof[data-id=\""+id+"\"]").siblings(".prec_tof").html('<img class="ico-client" src="images/photos/'+id+'.jpg" />');
+								 
+		nb_done++;
+	}	 
+	
+	avancement+=pas;
+	$('#progress_bar .ui-progress').animateProgress(avancement, function() {});
+	 
+	$("#results").text(nb_done+'/'+nb_tot+' éléments mis à jours');
+}
+
 function upload_pict()
 {
-	var upload_avancement = 0;
-	var nb_uploaded = 0;
-	var nb_to_upload = nb_found;
-	var upload_pas = 100/nb_to_upload;
 	
+	avancement = 0;
+	nb_tot = nb_done;
+	nb_done = 0;
+	pas=100/nb_tot;
+
 	$('#progress_bar .ui-progress').css('width','0');
 	$('.ui-label').css({'display':'block','cursor':'normal'}).unbind("click");
 
@@ -88,81 +165,21 @@ function upload_pict()
 		if (($(this).html() != '') && ($("input",this).prop("checked") == true))
 		{
 			var my_fb_id = $(this).siblings(".new_tof").children("img").attr("src").split('/')[3];
-			$.ajax({
-				url: "image_upload.php",
-				data   : {id:my_user_id,fb_id:my_fb_id},
-				  cache: false,
-				  dataType: "json",
-				error : function(request, error) {          
-						 $("#clients tr .new_tof[data-id=\""+my_id+"\"]").html('error');
-						},
-				  success: function(data) {    
-					
-							if (data.code_erreur == "0")
-							{
-								  $("#clients tr .new_tof[data-id=\""+my_user_id+"\"]").siblings(".checkbox").html('<img src="images/icones/icons/accept.png" />');
-								  $("#clients tr .new_tof[data-id=\""+my_user_id+"\"]").siblings(".prec_tof").html('<img class="ico-client" src="images/photos/'+my_user_id+'.jpg" />');
-								 
-								 nb_uploaded++;
-							}	 
-								 upload_avancement+=upload_pas;
-								 $('#progress_bar .ui-progress').animateProgress(upload_avancement, function() {});
-								 
-								 
-								 $("#results").text(nb_uploaded+'/'+nb_to_upload+' éléments mis à jours');
-					
-						 }
-					
-			  });
 			
+			var GET_arg = new Array();
+			GET_arg['id']=my_user_id;
+			GET_arg['fb_id']=my_fb_id;
+			ajax_url("image_upload.php", GET_arg, upload_callback, upload_error);
 		}
 	});
 }
 
 function search_pict(my_id)
 {
-	$.ajax({
-		url: "image_search.php",
-        data   : {id:my_id},
-          cache: false,
-          dataType: "json",
-        error : function(request, error) {          
-                 $("#clients tr .new_tof[data-id=\""+my_id+"\"]").html('error');
-				 avancement+=pas;
-				 $('#progress_bar .ui-progress').animateProgress(avancement, function() {});
-                },
-          success: function(data) {    
-			if (data.code_erreur == "0")
-			{
-                 $("#clients tr .new_tof[data-id=\""+my_id+"\"]").html('<img src="https://graph.facebook.com/'+data.reponse+'/picture" title="Nouvelle photo" />').siblings(".checkbox").html('<input type="checkbox" checked="checked" />');     
-				 
-				 nb_found++;
-			}	 
-				 avancement+=pas;
-				 $('#progress_bar .ui-progress').animateProgress(avancement, function() {});
-				 
-				 
-				 $("#results").text(nb_found+'/'+nb_tof+' éléments trouvés');
-				 
-				 if (avancement >= 100)
-				 $('#progress_bar .ui-progress').animateProgress(avancement, function() 
-				 {
-					$('.ui-label').delay(1500).text('Clickez-ici pour télécharger les photos').css({'display':'block','cursor':'pointer'}).click(function(){
-						upload_pict();
-					});
-				});
-                 }
-			
-      });
+	var GET_arg = new Array();
+	GET_arg['id']=my_id;
+	ajax_url("image_search.php", GET_arg, search_callback, search_error);
 }
 
-$(function() {
-  // Hide the label at start
-  $('#progress_bar .ui-progress .ui-label').hide();
-  // Set initial value
-  $('#progress_bar .ui-progress').css('width', '0%');
 
-  
-  
-});
 </script>
