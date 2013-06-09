f7357f42813ec74b82808f4ca903232858738de8
Charles BOCHET <pichnett.net@gmail.com>
2013-06-09 21:39:53 +0200

    Structuration de traitement.php

--- traitement.php
+++ traitement.php
@@ -1,21 +1,88 @@
 <?php
+//Application JSON
+header('Content-type: application/json'); 
+
+//Codes Erreurs
 define('AJAX_OK',0);
 define('AJAX_UNKNOW_FAIL',1);
 define('AJAX_NOT_IMPLEMENTED',2);
+define('UNDEFINED_ACTION',100);
+define('INVALID_ACTION',101);
 
- //if ($_GET['action'] == "ChercherDansMaBaseDeDonnees") { ChercherDansMaBaseDeDonnees(); }
+//Initialisations
+$erreur=-1;
+$reponse='';
 
-	$erreur = 0;
-	$reponse = "Commande passée avec succès pour l'élève dont l'id est : ".$_GET['id']." !";
- 
-$erreur = AJAX_NOT_IMPLEMENTED;
-$reponse = "Cette fonction n'a pas encore été implémentée !";
+//Include pour l'objet $bdd notamment
+include_once 'inclus/tete.inc.php';
 
-  header('Content-type: application/json');   
+/*/////////////////////////////FONCTIONS/////////////////////////////*/
+//Prendre une nouvelle commande
+function new_order()
+{
+	global $erreur;
+	global $reponse;
+	
+	$erreur = AJAX_OK;
+	$reponse = "Commande passée avec succès pour l'élève dont l'id est : xx !";
+}
+
+//Ajouter du liquide à un élève
+function add_cash()
+{
+	global $erreur;
+	global $reponse;
+	
+	$erreur = AJAX_OK;
+	$reponse = "Le solde de l'élève a bien été augmenté";
+}
+
+//Annuler une commande
+function cancel()
+{
+	global $erreur;
+	global $reponse;
+	
+	$erreur = AJAX_OK;
+	$reponse = "Commande annulée avec succès";
+}
+
+/*/////////////////////////////CORPS/////////////////////////////*/
+if (isset($_GET['action']))
+{
+	switch ($_GET['action']) 
+	{
+		case "order":
+			new_order();
+			break;
+		case "add_cash":
+			add_cash();
+			break;
+		case "cancel":
+			cancel();
+			break;
+		default:
+		   $erreur = INVALID_ACTION;
+		   $reponse = "Action incorrecte";
+	}
+}
+else
+{
+	$erreur = UNDEFINED_ACTION;
+	$reponse = "Action indéfinie";
+}
+
+//A ôter dès que les fonctions seront correctement implémentées
+if ($erreur == AJAX_OK)
+{
+	$erreur = AJAX_NOT_IMPLEMENTED;
+	$reponse = "Cette fonction n'a pas encore été implémentée !";
+}
+
   ?>
-  {
-		"code_erreur": "<?php echo $erreur; ?>",
-		"reponse": "<?php echo $reponse;?>"
-  }                                                         
+{
+	"code_erreur": "<?php echo $erreur; ?>",
+	"reponse": "<?php echo $reponse;?>"
+}                                                         
 
 
