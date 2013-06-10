7cfaa3654367abf3ab94840b93830d0adf8a53ee
Mickaël Bergem <mickael@securem.eu>
2013-06-11 01:17:48 +0200

    Patch de mise à jour de la BDD vers la v0.3

---
+++ admin/patch-BDD-v0.3.sql
@@ -0,0 +1,7 @@
+ALTER TABLE clients
+  ADD `promo` varchar(5) COLLATE utf8_bin NOT NULL,
+  ADD `distinction` enum('pookie','petit joueur','habitue','bide-a-biere','pochtron','bitumeux','roidufoyer','legende') COLLATE utf8_bin NOT NULL DEFAULT 'pookie',
+  ADD `litres_bus` decimal(6,2) NOT NULL,
+  ADD `commentaire` text COLLATE utf8_bin NOT NULL;
+
+ALTER TABLE commandes ADD  `id_produit` smallint(5) NOT NULL, ADD  `qtte_produit` int(11) NOT NULL, DROP commande;
