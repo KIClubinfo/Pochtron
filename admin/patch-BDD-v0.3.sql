9f500c88f3e0deb67fa09428ea6b573df6f5e5cb
Mickaël Bergem <mickael@securem.eu>
2013-06-11 10:13:37 +0200

    Bugix #2 pour la même chose.

--- admin/patch-BDD-v0.3.sql
+++ admin/patch-BDD-v0.3.sql
@@ -6,8 +6,7 @@ ALTER TABLE clients
 
 ALTER TABLE commandes ADD  `id_produit` smallint(5) NOT NULL, ADD  `qtte_produit` int(11) NOT NULL, DROP commande;
 
-ALTER TABLE bar DROP id,
-		ADD `id` enum('caisse','version') NOT NULL;
+ALTER TABLE `bar` CHANGE `id` `id` ENUM( 'caisse', 'version' ) NOT NULL;
 
 INSERT INTO `bar` (`id`, `val`) VALUES
 ('version', '0.3');
