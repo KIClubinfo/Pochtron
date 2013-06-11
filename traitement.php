60a72061dddd0b168134145dfab1a7c73e22665a
Charles BOCHET <pichnett.net@gmail.com>
2013-06-11 12:04:30 +0200

    Mise a jour traitement.php

--- traitement.php
+++ traitement.php
@@ -10,7 +10,8 @@ define('UNDEFINED_ACTION',100);
 define('INVALID_ACTION',101);
 define('UNDEFINED_ID',200);
 define('INVALID_ID',201);
-define('BAD_REQUEST',300);
+define('EMPTY_ORDER',300);
+define('INVALID_ORDER',301);
 
 //Initialisations
 $erreur=-1;
@@ -20,6 +21,20 @@ $reponse='';
 include_once 'inclus/tete.inc.php';
 
 /*/////////////////////////////FONCTIONS/////////////////////////////*/
+//Conversion de la chaîne de commande en un tableau
+function parse_order($string_order)
+{
+	$array_temp = explode(',',$string_order);
+	$array_order = array();
+	
+	for ($i=1;$i<count($array_temp);$i++)
+	{
+		$array_order[$i] = explode(':',$array_temp[$i]);
+	}
+	
+	return $array_order;
+}
+
 //Prendre une nouvelle commande
 function new_order()
 {
@@ -48,10 +63,24 @@ function new_order()
 		
 		$sql->rek( 'UPDATE clients SET solde=\''.($eleve['solde']-1).'\' WHERE id=\''.$_GET['id'].'\'');
 		
+		if (!isset($_GET['consom']))
+		{
+			$erreur=EMPTY_ORDER;
+			$reponse="Commande vide";
+			return;
+		}
+		
+		$order = parse_order(htmlspecialchars($_GET['consom'], ENT_QUOTES, 'UTF-8'));
+		
+		if (count($order) < 2)
+		{
+			$erreur=INVALID_ORDER;
+			$reponse="Commande invalide";
+			return;
+		}
+		
 		$erreur = AJAX_OK;
 		$reponse = "Commande de ".$eleve['prenom']." ".$eleve['nom']." passée avec succès. Nouveau solde : ".($eleve['solde']-1);
-		
-	
 	}
 }
 
