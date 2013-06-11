c6585a4bd5a9f868d5e1a614a10a64eaa041f230
Mickaël Bergem <mickael@securem.eu>
2013-06-11 23:50:11 +0200

    Gestion des sessions pour le suivi des commandes.

--- suivi.php
+++ suivi.php
@@ -1,5 +1,6 @@
 <?php
 define('page_titre', "Historique des commandes &bull; Caisse Foyer");
+define('_NBH_SESSION', 6); // Nombre d'heure sans commande avant le début d'une nouvelle session
 
 include_once 'inclus/zoneadmin.inc.php';
 
@@ -11,15 +12,26 @@ include_once 'inclus/tete.html.php';
   <div class="central-haut bloc-largeur journal">
    <a href="./administration.php" class="maison">Retour</a>
     <h1>Journal des consommations</h1>
-    <table class="lignes events">
+    <table class="lignes events suivi">
     <thead><tr><th>Date</th><th>Client</th><th>Consommation</th></tr></thead>
     <tbody>
 <?php
-$sql->rek( "SELECT a.`timestamp`,a.`qtte_produit`,b.nom as `nom_produit`,CONCAT(c.prenom,' ',c.nom) as `nom_client` FROM commandes as a, clients as c, produits as b WHERE a.id_user = c.id AND a.id_produit = b.id ORDER BY timestamp,qtte_produit DESC" );
+$sess = 'a'; // Bascule la couleur du fond
+$last_date = time() + _NBH_SESSION*3600;
+$mois = Array('','Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
+
+$sql->rek( "SELECT a.`timestamp`,a.`qtte_produit`,b.nom as `nom_produit`,CONCAT(c.prenom,' ',c.nom) as `nom_client` FROM commandes as a, clients as c, produits as b WHERE a.id_user = c.id AND a.id_produit = b.id ORDER BY timestamp DESC,qtte_produit ASC" );
 while($a = $sql->fetch()){
-  $heure = date('H:i:s d/m/y',strtotime($a['timestamp']));
-  $session = date('d/m',strtotime($a['timestamp']));
-  echo "<tr><td><time>$heure</time> (session : $session)</td><td>{$a['nom_client']}</td><td>{$a['qtte_produit']} {$a['nom_produit']}</td></tr>";
+  $t = strtotime($a['timestamp']);
+  $heure = date('H:i:s (d/m)',$t);
+  $session = date('d',$t) . ' ' . $mois[intval(date('m',$t))];
+  
+  if($last_date - strtotime($a['timestamp']) > _NBH_SESSION*3600 ){ // On change de session
+    $sess = ($sess == 'a') ? 'b' : 'a';
+    echo "<tr class='spacer-$sess'><td colspan='3'>Nouvelle session : <strong>$session</strong></td></tr>";
+    $last_date = strtotime($a['timestamp']);
+  }
+  echo "<tr class='sess-$sess'><td><time>$heure</time></td><td>{$a['nom_client']}</td><td>{$a['nom_produit']} (<strong>{$a['qtte_produit']}</strong>)</td></tr>";
 }
 
 ?></tbody></table>
