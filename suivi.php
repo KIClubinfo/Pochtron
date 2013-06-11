7cbe392df33717127999abb794bd640c9e6bc312
Mickaël Bergem <mickael@securem.eu>
2013-06-12 00:25:15 +0200

    Correction CSS +meilleur tri des sessions de conso

--- suivi.php
+++ suivi.php
@@ -12,29 +12,41 @@ include_once 'inclus/tete.html.php';
   <div class="central-haut bloc-largeur journal">
    <a href="./administration.php" class="maison">Retour</a>
     <h1>Journal des consommations</h1>
-    <table class="lignes events suivi">
-    <thead><tr><th>Date</th><th>Client</th><th>Consommation</th></tr></thead>
+    
     <tbody>
 <?php
-$sess = 'a'; // Bascule la couleur du fond
-$last_date = time() + _NBH_SESSION*3600;
+$thead = '<table class="lignes events suivi"><thead><tr><th>Date</th><th>Client</th><th>Consommation</th></tr></thead>';
+$tend = '</tbody></table>';
+
+echo $thead;
+
+$sess = '0'; // Bascule la couleur du fond
+$last_date = 0;
 $mois = Array('','Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
 
-$sql->rek( "SELECT a.`timestamp`,a.`qtte_produit`,b.nom as `nom_produit`,CONCAT(c.prenom,' ',c.nom) as `nom_client` FROM commandes as a, clients as c, produits as b WHERE a.id_user = c.id AND a.id_produit = b.id ORDER BY timestamp DESC,qtte_produit ASC" );
+$sql->rek( "SELECT a.`timestamp`,a.`qtte_produit`,b.nom as `nom_produit`,CONCAT(c.prenom,' ',c.nom) as `nom_client` FROM commandes as a, clients as c, produits as b WHERE a.id_user = c.id AND a.id_produit = b.id ORDER BY DATE(SUBTIME(a.timestamp,'0 6:0:0')) DESC, timestamp ASC, qtte_produit ASC" );
+
 while($a = $sql->fetch()){
   $t = strtotime($a['timestamp']);
   $heure = date('H:i:s (d/m)',$t);
   $session = date('d',$t) . ' ' . $mois[intval(date('m',$t))];
   
-  if($last_date - strtotime($a['timestamp']) > _NBH_SESSION*3600 ){ // On change de session
+  if(abs($last_date - strtotime($a['timestamp'])) > _NBH_SESSION*3600 or $sess=='0'){ // On change de session
+    
+    if($sess!='0'){
+	echo $tend;
+	
+	echo $thead;
+    }
     $sess = ($sess == 'a') ? 'b' : 'a';
-    echo "<tr class='spacer-$sess'><td colspan='3'>Nouvelle session : <strong>$session</strong></td></tr>";
+    echo "<tr class='spacer-$sess'><td colspan='3'>Session : <strong>$session</strong></td></tr>";
     $last_date = strtotime($a['timestamp']);
   }
   echo "<tr class='sess-$sess'><td><time>$heure</time></td><td>{$a['nom_client']}</td><td>{$a['nom_produit']} (<strong>{$a['qtte_produit']}</strong>)</td></tr>";
 }
 
-?></tbody></table>
+echo $tend;
+?>
   </div>
 <?php
 include 'inclus/pied.html.php';
