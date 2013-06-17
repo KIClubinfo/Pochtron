6a532e902e143e108b906fb8ede6dcfac1de5195
Mickaël Bergem <mickael@securem.eu>
2013-06-17 23:04:21 +0200

    Bugfix dans la MAJ SQL

--- admin/patch-BDD-v0.5.sql
+++ admin/patch-BDD-v0.5.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `bar` CHANGE `id` `id` ENUM( 'caisse', 'version', 'PIN1', 'PIN2' ) NOT NULL;
 
 INSERT INTO `bar` (`val` ,`id`) VALUES ('120393', 'PIN1'), ('011091', 'PIN2');
+UPDATE `bar` SET `val` = '0.5' WHERE `id` = 'version';
