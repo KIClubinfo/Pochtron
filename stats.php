21bb4ad2e01bbabc8b17662df4cc6fa108b857d9
Mickaël Bergem <mickael@securem.eu>
2013-06-18 01:39:02 +0200

    Mise en place de différentes statistiques

--- stats.php
+++ stats.php
@@ -9,12 +9,44 @@ include_once 'inclus/tete.html.php';
    <a href="./index.php" class="maison">Retour</a>
     <h1>Statistiques</h1>
     <div class="notif estompe">Vous pouvez ici consulter l'évolution de la consommations des différents produits depuis le début du logiciel.</div>
-    <div class="darkbox moitie" id="container">
+    <div class="darkbox" id="consos"></div>
+    <table class="lignes" id="bestdrunks">
+    <caption><span>Meilleurs buveurs du foyer</span></caption>
+    <thead>
+    <tr><th>Rang</th><th>Nom</th><th>Litres ingérés</th></tr>
+    </thead>
+    <tbody>
+    <?php
+$sql->rek( "select CONCAT(prenom,' ',nom) as nom, promo, litres_bus from clients WHERE active='activé' ORDER BY litres_bus DESC LIMIT 10;" );
+$r = 1;
+while($a = $sql->fetch()){
+    echo '<tr><td>'.($r++).'</td><td>'.$a['nom'].(empty($a['promo']) ? '':" {$a['promo']}]").'</td><td>'.$a['litres_bus'].'</td></tr>';
+}
+    ?>
+    </tbody>
+    </table>
+    <table class="lignes" id="bestproducts">
+    <caption><span>Meilleurs ventes du foyer</span></caption>
+    <thead>
+    <tr><th>Rang</th><th>Nom</th><th>Produits vendus</th></tr>
+    </thead>
+    <tbody>
+    <?php
+$sql->rek( " select count(*) as nb,p.nom from commandes as c, produits as p where c. id_produit = p.id  group by c.id_produit order by count(*) DESC LIMIT 10;" );
+$r = 1;
+while($a = $sql->fetch()){
+    echo '<tr><td>'.($r++).'</td><td>'.$a['nom'].'</td><td>'.$a['nb'].'</td></tr>';
+}
+    ?>
+    </tbody>
+    </table>
+    <div class="darkbox" id="consos2"></div>
     </div>
 <?php
 
 // Affichage du graphe
 $consommations = Array();
+$litres = Array();
 
 // Remplissage du graphe (tous types confondus)
 $sql->rek( "SELECT DATE(SUBTIME(a.timestamp,'0 6:0:0')) as date,a.`qtte_produit`,CONCAT(b.nom,' ',b.vol,'L') as `nom_produit` FROM commandes as a, produits as b WHERE  a.id_produit = b.id GROUP BY a.id_produit,DATE(SUBTIME(a.timestamp,'0 6:0:0')) ORDER BY DATE(SUBTIME(a.timestamp,'0 6:0:0')) DESC, a.id_produit;" );
@@ -24,8 +56,15 @@ while($a = $sql->fetch()){
     // Et on ajoute le point du graphe correspondant
     $consommations[$a['nom_produit']][] = '[Date.UTC('.date('Y,m,d',strtotime($a['date'])).'),'.$a['qtte_produit'].']';
 }
+
+// Remplissage du graphe (tous types confondus)
+$sql->rek( "select sum( p.vol * c. qtte_produit ) as volume, DATE(SUBTIME(c.timestamp,'0 6:0:0')) as date from commandes as c, produits as p WHERE c.id_produit = p.id GROUP BY DATE(SUBTIME(c.timestamp,'0 6:0:0')) order by c.timestamp;" );
+while($a = $sql->fetch()){
+    // On ajoute le point du graphe correspondant
+    $litres[] = '[Date.UTC('.date('Y,m,d',strtotime($a['date'])).'),'.$a['volume'].']';
+}
+
 ?>
-  </div>
     <script>
     Highcharts.setOptions({
 	lang: {
@@ -36,18 +75,18 @@ while($a = $sql->fetch()){
 		weekdays: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
 	}
 });
-    $('#container').highcharts({
+    $('#consos').highcharts({
         chart: {
             type: 'spline'
         },
         title: {
-            text: ''
+            text: 'Consommation par produits'
         },
         subtitle: {
-            text: ''
+            text: 'Ils pourraient pas tous boire de la kro, comme tout le monde ?'
         },
         legend: {
-            enabled: false
+//             enabled: false
         },
         xAxis: {
 	    type: 'datetime',
@@ -81,6 +120,43 @@ while($a = $sql->fetch()){
         ?>
         
         ]
+    });
+    
+    
+    $('#consos2').highcharts({
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Nombre de litres de boisson ingérés'
+        },
+        subtitle: {
+            text: ''
+        },
+        legend: {
+            enabled: false
+        },
+        xAxis: {
+	    type: 'datetime',
+	    dateTimeLabelFormats: {
+                    month: '%b %e',
+                    year: '%b'
+                }
+        },
+        yAxis: {
+            title: {
+                text: 'Volume en Litres'
+            },
+            min: 0
+        },
+        tooltip: {
+	    dateTimeLabelFormats: {
+                    month: '%b %e',
+		    day: '%A %e %B',
+                    year: '%b'
+            }
+        },
+        series: [{ name: 'Volume ingéré', data: [<?php echo implode(',',$litres); ?>]}]
     });
 </script>
 <?php
